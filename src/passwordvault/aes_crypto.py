540bd289fc53d2ce20b7bfe0f045a1467637a6d4
Humaira Afrin <humaira.afrin.stockholm@gmail.com>
2025-04-13 15:10:18 +0200

    Hashing using the SHA-2 hashing algortitm

--- src/passwordvault/aes_crypto.py
+++ src/passwordvault/aes_crypto.py
@@ -3,9 +3,18 @@ from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.backends import default_backend
 import os
 import base64
+import hashlib
+
 
 # AES block size i bitar
 BLOCK_SIZE = 128
+
+
+# Hash the master password (SHA-256 hashing algoritm) ---
+def hash_password(password: str) -> str:
+    hashed = hashlib.sha256(password.encode()).hexdigest()
+    return hashed
+
 
 def encrypt(plaintext: str, key: bytes) -> str:
     iv = os.urandom(16)  # 16 byte IV för AES
@@ -33,3 +42,8 @@ def decrypt(encoded_data: str, key: bytes) -> str:
     decrypted_data = unpadder.update(decrypted_padded) + unpadder.finalize()
 
     return decrypted_data.decode()
+
+
+
+
+
